aa11fddeeabfc8fad89744ce5c0ba63c89c884f0
CaoBin <bin.cao@beecloud.com>
2017-05-25 16:33:23 +0800

    update test to com

--- src/main/resources/mbg/mbg.sh
+++ src/main/resources/mbg/mbg.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #https://github.com/Cao-Bin/mybatis-generator-core-chinese-annotation-1.3.5.git
-rm -rf test
+rm -rf com
 java -classpath mapper-3.4.0.jar:mysql-connector-java-5.1.21.jar:mybatis-generator-core-1.3.5-chinese-annotation.jar org.mybatis.generator.api.ShellRunner -configfile MBGConfig.xml -overwrite
